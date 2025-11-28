0df3210ab88069922aaa3f3de3aac7a8a67b1a8b
zronowa17-maker <zronowa17@gmail.com>
2025-11-28 19:47:22 +0800

    copy the given code

--- FrmClubRegistration/FrmClubRegistration.cs
+++ FrmClubRegistration/FrmClubRegistration.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace FrmClubRegistration
 {
     public partial class FrmClubRegistration : Form
     {
+        private SqlConnection sqlconnection;
+        private SqlCommand sqlcommand;
         public FrmClubRegistration()
         {
             InitializeComponent();
@@ -26,5 +29,34 @@ namespace FrmClubRegistration
         {
 
         }
+        public bool RegisterStudent(int ID, long StudentID, string FirstName, string MiddleName, string LastName, int Age, string Gender, string Program)
+        {
+            sqlcommand = new SqlCommand("INSERT INTO ClubMembers VALUES(@ID, @StudentID, @FirstName, @MiddleName, @LastName, @Age, @Gender, @Program)", sqlconnection);
+            sqlcommand.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+            sqlcommand.Parameters.Add("@StudentID", SqlDbType.BigInt).Value = StudentID; 
+            sqlcommand.Parameters.Add("@RegistrationID", SqlDbType.BigInt).Value = StudentID; 
+            sqlcommand.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = FirstName;
+            sqlcommand.Parameters.Add("@MiddleName", SqlDbType.VarChar).Value = MiddleName;
+            sqlcommand.Parameters.Add("@LastName", SqlDbType.VarChar).Value = LastName;
+            sqlcommand.Parameters.Add("@Age", SqlDbType.Int).Value = Age;
+            sqlcommand.Parameters.Add("@Gender", SqlDbType.VarChar).Value = Gender;
+            sqlcommand.Parameters.Add("@Program", SqlDbType.VarChar).Value = Program;
+            sqlconnection.Open();
+            sqlcommand.ExecuteNonQuery();
+            sqlconnection.Close();
+
+            return true;
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+        
